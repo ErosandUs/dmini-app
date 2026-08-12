30cb3014293fa52d4500d1c4305264eb9a8cb5a0
Валентин <a_valek@mail.ru>
2026-08-12 15:52:12 +0300

    go

--- js/app.js
+++ js/app.js
@@ -299,7 +299,7 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             const absoluteMediaUrl = new URL(imagePath, window.location.href).toString();
             const params = {
-                text: "Получи своё послание от Вселенной! ✨ @Djamilia_Kha", 
+                text: "Получи своё послание от Вселенной! ✨", 
                 widget_link: { 
                     url: BOT_LINK, 
                     name: "Получить послание 💫" 
